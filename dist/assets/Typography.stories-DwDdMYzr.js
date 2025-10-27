ff6ce8f6be24f9c1f30bc2b3251b89a5d1f83631
daniel-winter <wind@xxxlgroup.com>
2025-10-27 09:53:08 +0100

    add storybook dist folder

---
+++ dist/assets/Typography.stories-DwDdMYzr.js
@@ -0,0 +1,19 @@
+import{r as m,e as t}from"./iframe-BFEqPYAy.js";import{T as H}from"./TokenHeader-D8-Cprlb.js";import{u as B,f as v,C as l,a as R}from"./useBrand-917ayicg.js";import"./preload-helper-Dp1pzeXC.js";const w="_wrapper_1fm4m_1",z="_size_1fm4m_5",G="_item_1fm4m_9",s={wrapper:w,size:z,item:G},T="kFontText",I="kFontTextHero",P="kFontTextTitle",x=({type:o=T})=>{const{tokens:i}=B(),O=m.useMemo(()=>v(i,o),[i,o]),p=Object.entries(O),N=e=>!e.startsWith(I)&&!e.startsWith(P),h=m.useMemo(()=>o===T?p.filter(([e])=>N(e)):p,[p,o]);return t.createElement("div",{className:s.wrapper},t.createElement(H,null),h.map(([e,c])=>t.createElement("div",{key:e,className:s.item},t.createElement("div",{className:s.size},t.createElement(l,{name:e,filter:c,hasBackground:!0})),t.createElement("div",{className:s.size},t.createElement(l,{name:e,filter:c,hasBackground:!0,prefix:"$"})),t.createElement("span",{style:{font:`var(${R(e,c)})`}},"Example Text"))))};x.__docgenInfo={description:"Typography is an integral part of our design system. Good typography helps to present pages clearly and efficiently to the user.",methods:[],displayName:"Typography",props:{type:{required:!1,tsType:{name:"union",raw:"typeof TOKEN_GROUP | typeof FONT_HERO | typeof FONT_TITLE",elements:[{name:"TOKEN_GROUP"},{name:"FONT_HERO"},{name:"FONT_TITLE"}]},description:"possible typography types",defaultValue:{value:"'kFontText'",computed:!1}}}};const U={title:"Foundation/Typography",component:x},r={tags:["!autodocs"],args:{type:"kFontTextHero"}},a={args:{type:"kFontTextTitle"}},n={args:{type:"kFontText"},argTypes:{type:{control:"select"}}};var d,y,u;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  args: {
+    type: 'kFontTextHero'
+  }
+}`,...(u=(y=r.parameters)==null?void 0:y.docs)==null?void 0:u.source}}};var f,g,F;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    type: 'kFontTextTitle'
+  }
+}`,...(F=(g=a.parameters)==null?void 0:g.docs)==null?void 0:F.source}}};var _,k,E;n.parameters={...n.parameters,docs:{...(_=n.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    type: 'kFontText'
+  },
+  argTypes: {
+    type: {
+      control: 'select'
+    }
+  }
+}`,...(E=(k=n.parameters)==null?void 0:k.docs)==null?void 0:E.source}}};const b=["HeroFont","TitleFonts","BodyFonts"];export{n as BodyFonts,r as HeroFont,a as TitleFonts,b as __namedExportsOrder,U as default};
